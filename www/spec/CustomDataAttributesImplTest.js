fbd335b23e7f97b24ea646c5491fbc0d622f8c1a
jbiehler <jbiehler@samhammer.de>
2013-12-24 16:09:23 +0100

    add another unit test for customdataattributes

--- www/spec/CustomDataAttributesImplTest.js
+++ www/spec/CustomDataAttributesImplTest.js
@@ -17,5 +17,20 @@ describe("CustomDataAttributesImpl:", function() {
 			$("#testbutton").click();
 			expect(content.loadIntoSection).toHaveBeenCalledWith("test3.html", "#test3");
 		});
+		
+		it("should set onClick only within specified div", function() {
+			var str = '<div id="withinDiv_spec2"><div data-cda-loadOnClick="yes" data-cda-location="test.html" data-cda-location-section="#test" id="testdiv"></div></div>'+
+					  '<div id="notWithinDiv_spec2"><a data-cda-loadOnClick="yes" data-cda-location="test2.html" data-cda-location-section="#test2" id="testa"></a></div>';
+					  
+			setFixtures(str);
+			content = jasmine.createSpyObj('Content', ['loadIntoSection']);
+			cda = new CustomDataAttributesImpl(content);
+			cda.setOnClickEvent("#withinDiv_spec2");
+			
+			$("#testdiv").click();
+			expect(content.loadIntoSection).toHaveBeenCalledWith("test.html", "#test");
+			$("#testa").click();
+			expect(content.loadIntoSection).not.toHaveBeenCalledWith("test2.html", "#test2");
+		});
 	});
 });
