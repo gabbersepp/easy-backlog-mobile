dd4222f3be8f12b27f250894330e4af23d0cd47d
jbiehler <jbiehler@samhammer.de>
2013-12-27 16:29:35 +0100

    localeStorage config can only store one key|value pair & after refreshing page EasyBacklogController has lost all information about eb key&accountid

--- www/js/impl/ConfigLocaleStorageImpl.js
+++ www/js/impl/ConfigLocaleStorageImpl.js
@@ -2,8 +2,8 @@ var ConfigLocaleStorageImpl = function() {
 }
 ConfigLocaleStorageImpl.prototype = Object.create(IConfig);
 ConfigLocaleStorageImpl.prototype.get = function(key, attr) {
-	return window.localStorage.getItem("key"+"attr");
+	return window.localStorage.getItem(key+attr);
 };
 ConfigLocaleStorageImpl.prototype.set = function(key, attr, value) {
-	window.localStorage.setItem("key"+"attr", value);
+	window.localStorage.setItem(key+attr, value);
 };
